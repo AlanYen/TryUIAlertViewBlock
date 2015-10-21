b2f7ffff8b6f6d62240c4531c490eec5f1735d01
AlanYen <alanyen.mobile@gmail.com>
2015-10-21 11:24:45 +0800

    Init Commit

---
+++ Podfile
@@ -0,0 +1,5 @@
+
+source 'https://github.com/CocoaPods/Specs.git'
+
+pod 'BlocksKit', '~> 2.2.5'
+pod 'UIAlertView+Blocks', '~> 0.9'
--- TryUIAlertViewBlock/ViewController.m
+++ TryUIAlertViewBlock/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import "BlocksKit+UIKit.h"
+#import "UIAlertView+Blocks.h"
 
 @interface ViewController ()
 
@@ -17,6 +19,35 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    [UIAlertView bk_showAlertViewWithTitle:@"訊息"
+                                   message:@"測試 bk_showAlertViewWithTitle"
+                         cancelButtonTitle:@"確定"
+                         otherButtonTitles:nil
+                                   handler:^(UIAlertView *alertView, NSInteger buttonIndex) {
+                                   }];
+
+    //
+    // 下面寫法會進入無窮迴圈,兩者只能擇一
+    //
+    
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"訊息"
+                                                    message:@"測試 UIAlertView+Blocks"
+                                                   delegate:nil
+                                          cancelButtonTitle:@"取消"
+                                          otherButtonTitles:@"刪除",nil];
+    
+    alert.tapBlock = ^(UIAlertView *alertView, NSInteger buttonIndex) {
+        
+        if (buttonIndex == alertView.firstOtherButtonIndex) {
+        }
+    };
+    
+    [alert show];
 }
 
 - (void)didReceiveMemoryWarning {
